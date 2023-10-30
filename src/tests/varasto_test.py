1675f6abac93bbfed42f8bcdec44daf1c2ed7f67
Venla R <venla.e.rantanen@gmail.com>
2023-10-30 15:40:10 +0200

    lisää poetry ja coverage, 100% testikattavuus

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,32 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_liikaa_ottaminen_palauttaa_kaiken_mita_on(self):
+        self.varasto.lisaa_varastoon(2)
+        saatu = self.varasto.ota_varastosta(6)
+        self.assertAlmostEqual(saatu, 2)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_liikaa_laittaminen_laittaa_tilavuuden_verran(self):
+        self.varasto.lisaa_varastoon(12)
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
+
+    def test_saldo_teksti_on_oikein(self):
+        saatu = self.varasto.__str__()
+        self.assertEqual(saatu, "saldo = 0, vielä tilaa 10")
+
+    def test_negatiivinen_lisäys_ei_onnistu(self):
+        varasto = Varasto(10, -5)
+        self.assertEqual(varasto.saldo, 0)
+
+        self.varasto.lisaa_varastoon(-5)
+        self.assertEqual(self.varasto.saldo, 0)
+
+    def test_negatiivinen_ottaminen_ei_onnistu(self):
+        saatu = self.varasto.ota_varastosta(-10)
+        self.assertEqual(saatu, 0)
+
+    def test_negatiivinen_tilavuus_ei_onnistu(self):
+        varasto = Varasto(-10)
+        self.assertEqual(varasto.tilavuus, 0)
